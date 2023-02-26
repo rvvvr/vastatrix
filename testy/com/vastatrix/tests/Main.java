88c5151798c122b823d84326b2c78c0a310bd762
rvvvr <me@visu.do>
2023-02-25 22:46:37 -0800

    just testing gpg sign worky agian

--- testy/com/vastatrix/tests/Main.java
+++ testy/com/vastatrix/tests/Main.java
@@ -5,6 +5,6 @@ import com.vastatrix.tests.MoreMath;
 
 class Main {
     public static void main(String[] args){
-        int a = Math.add(Math.instantiate(), 4);
+        int a = Math.add(Math.instantiate(), 5);
     }
 }
