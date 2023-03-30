1beae64331e5ad21f626b313869f330349f66fb4
rvvvr <me@visu.do>
2023-03-30 12:44:58 -0700

    generating big strong code from small weak macro

---
+++ vtx-std/src/array.rs
@@ -0,0 +1,5 @@
+use vtx_jbridge::class;
+
+class!(
+
+);
---
+++ vtx-std/src/object.rs
@@ -0,0 +1,21 @@
+use vtx_jbridge::class;
+
+class!(
+    package java.lang;
+
+    public class Object {
+        //method takes the same arguments as Frame::exec
+        static "<init>", "()V" {
+            Argument::new(0, MethodType::Void)
+        }
+
+        static "MeepMorp", "(II)I" {
+            Argument::new(0, MethodType::Void)
+        }
+
+        static "MeepMorp", "(I)I" {
+            Argument::new(0, MethodType::Void)
+        }
+
+    }
+);
